e6d8c5aad29917420319028a812c1a63613f6b18
SergWhiteBear <saghin.sergey@mail.ru>
2023-10-30 20:58:10 +0500

    Simple voice recorder with two buttons

---
+++ app/src/main/java/com/example/android_dictaphone_decoder/DictaphoneActivity.kt
@@ -0,0 +1,91 @@
+package com.example.android_dictaphone_decoder
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Build
+import android.os.Environment
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.IOException
+
+class DictaphoneActivity(private val activity: ComponentActivity) {
+
+    private var mediaRecorder: MediaRecorder? = null
+    private var mediaPlayer: MediaPlayer? = null
+    private var outputFilePath: String? = null
+    private var counterRecord: Int? = 0
+
+    // проверка на разрешения доступа к микрофону и к записи в хранилище
+    fun checkPermission(context: Context) {
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            val permissions = arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(activity, permissions,0)
+        }
+    }
+
+    fun startRecording(context: Context) {
+        mediaRecorder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) MediaRecorder(context) else MediaRecorder()
+        outputFilePath = createOutputFile().absolutePath
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            mediaRecorder = MediaRecorder(context).apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+                setOutputFile(getOutputFilePath(context))
+                try{
+                    prepare()
+                    start()
+                } catch (e: IOException)
+                {
+                    // перехват
+                }
+
+            }
+        }
+    }
+
+    private fun createOutputFile(): File {
+        val directory = Environment.getExternalStorageDirectory()
+        val folder = File(directory, "MyRecorder")
+        if (!folder.exists()) {
+            folder.mkdirs()
+        }
+        counterRecord = counterRecord!! + 1
+        return File(folder, "recording_${counterRecord}.3gp")
+    }
+
+    fun stopRecording() {
+        mediaRecorder?.apply {
+            stop()
+            release()
+        }
+        mediaRecorder = null
+    }
+    fun startPlaying(context: Context) {
+        mediaPlayer = MediaPlayer().apply {
+            setDataSource(getOutputFilePath(context))
+            prepare()
+            start()
+        }
+    }
+    fun stopPlaying() {
+        mediaPlayer?.apply {
+            stop()
+            release()
+        }
+    }
+    private fun getOutputFilePath(context: Context): String {
+        val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_MUSIC)
+        val fileName = "recording_${counterRecord}.3gp"
+        val file = File(storageDir, fileName)
+        return file.absolutePath
+    }
+}
--- app/src/main/java/com/example/android_dictaphone_decoder/MainActivity.kt
+++ app/src/main/java/com/example/android_dictaphone_decoder/MainActivity.kt
@@ -3,44 +3,103 @@ package com.example.android_dictaphone_decoder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.android_dictaphone_decoder.ui.theme.AndroiddictaphonedecoderTheme
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
+
+/*
+*
+*   Простой диктофон использующий MediaRecorder и имеющий 2 кнопки
+*   начать запись/закончить запись и прослушать запись/закончить прослушивание
+*
+* */
 class MainActivity : ComponentActivity() {
+    private val dictaphoneActivity = DictaphoneActivity(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            AndroiddictaphonedecoderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            DictaphoneApp()
+        }
+    }
+
+    @Composable
+    fun DictaphoneApp() {
+        var isRecording by remember { mutableStateOf(false) }
+        var isPlaying by remember { mutableStateOf(false) }
+
+        val context = LocalContext.current
+
+        // Проверка разрешений и запрос их у пользователя
+        DisposableEffect(Unit) {
+            dictaphoneActivity.checkPermission(context)
+            onDispose { }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = {
+                    if (!isRecording) {
+                        dictaphoneActivity.startRecording(context)
+                    } else {
+                        dictaphoneActivity.stopRecording()
+                    }
+                    isRecording = !isRecording
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(text = if (isRecording) "Stop Recording" else "Start Recording")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    if (!isPlaying) {
+                        dictaphoneActivity.startPlaying(context)
+                    } else {
+                        dictaphoneActivity.stopPlaying()
+                    }
+                    isPlaying = !isPlaying
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(text = if (isPlaying) "Stop Playing" else "Start Playing")
             }
         }
     }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroiddictaphonedecoderTheme {
-        Greeting("Android")
-    }
-}
+
+
+
+
